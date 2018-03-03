01ce181fcb03a0f15819c841713a123943f7956c
Rumen Filkov <rumen.filkov@gmail.com>
2018-03-03 09:48:59 +0100

    updated to the latest arkit-plugin (not tested yet)

--- Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/NativeInterface/ARSessionNative.mm
+++ Assets/UnityARKitPlugin/Plugins/iOS/UnityARKit/NativeInterface/ARSessionNative.mm
@@ -691,7 +691,7 @@ static CGAffineTransform s_CurAffineTransform;
             memcpy(s_UnityPixelBuffers.pUVPixelBytes, baseAddress, numBytes);
         }
         
-        CVPixelBufferUnlockBaseAddress(pixelBuffer, 0);
+        CVPixelBufferUnlockBaseAddress(pixelBuffer, kCVPixelBufferLock_ReadOnly);
     }
     
     id<MTLTexture> textureY = nil;
